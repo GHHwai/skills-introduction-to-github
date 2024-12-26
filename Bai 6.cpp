bb8026743e47efea9d05b420f6f2bb2b97456188
GHHwai <hoduongquocthong1234@gmail.com>
2024-12-26 18:04:13 +0700

    Add files via upload

---
+++ Bai 6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int payAmount=2200, payPeriods=26, annualPay;
+	annualPay=payAmount*payPeriods;
+	cout <<"Annual pay is: "<<annualPay;
+	return 0;
+}
