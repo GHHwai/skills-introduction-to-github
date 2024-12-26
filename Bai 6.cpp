9cffd3854adb2777b387b68a23d4c1bb38aa3fd0
GHHwai <hoduongquocthong1234@gmail.com>
2024-12-26 18:08:23 +0700

    Bai 6.cpp

--- Bai 6.cpp
+++ Bai 6.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int payAmount=2200, payPeriods=26, annualPay;
-	annualPay=payAmount*payPeriods;
-	cout <<"Annual pay is: "<<annualPay;
-	return 0;
+	cout<<"No bruh";
 }
